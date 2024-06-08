1c1455b526f24e3212463f571d41780407549efb
ThisisWt <2983586123@qq.com>
2024-06-08 11:45:13 +0800

    update导入模型

--- 导入模型/app4.py
+++ 导入模型/app4.py
@@ -1,38 +1,49 @@
 # app.py
-from flask import Flask, request, send_from_directory, render_template
+from flask import Flask, request, send_from_directory, render_template, jsonify
 from werkzeug.utils import secure_filename
 import os
 
-app = Flask(__name__)
-
-# 上传模型文件的存储目录
 UPLOAD_FOLDER = 'uploads'
+STATIC_FOLDER = 'static'
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['STATIC_FOLDER'] = STATIC_FOLDER
 app.config['SERVER_NAME'] = 'localhost:5003'
 
 @app.route('/')
 def app4_home():
     return render_template('index.html')
 
-# 处理文件上传请求
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return 'No file part'
+        return jsonify({'error': 'No file part'})
     
     file = request.files['file']
     if file.filename == '':
-        return 'No selected file'
+        return jsonify({'error': 'No selected file'})
     
     if file:
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return 'File uploaded successfully'
+        file_url = f"/uploads/{filename}"
+        return jsonify({'url': file_url})
 
-# 提供已上传的模型文件
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/view_model')
+def view_model():
+    return render_template('view_model.html')
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory(app.config['STATIC_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
