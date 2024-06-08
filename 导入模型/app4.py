9fcea3b1b20b3dbc03df36f19972e6ac8d21052e
Satanovo <164618012+Satanovo@users.noreply.github.com>
2024-06-08 16:38:31 +0800

    导入模型

--- 导入模型/app4.py
+++ 导入模型/app4.py
@@ -1,6 +1,6 @@
-# app.py
 from flask import Flask, request, send_from_directory, render_template, jsonify
 from werkzeug.utils import secure_filename
+from flask_cors import CORS
 import os
 
 UPLOAD_FOLDER = 'uploads'
@@ -9,41 +9,48 @@ STATIC_FOLDER = 'static'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=STATIC_FOLDER)
+CORS(app)  # 启用CORS
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['STATIC_FOLDER'] = STATIC_FOLDER
 app.config['SERVER_NAME'] = 'localhost:5003'
 
+
 @app.route('/')
-def app4_home():
+def home():
     return render_template('index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
-    
+
     file = request.files['file']
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
-    
+
     if file:
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         file_url = f"/uploads/{filename}"
         return jsonify({'url': file_url})
 
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 @app.route('/view_model')
 def view_model():
     return render_template('view_model.html')
 
+
 @app.route('/static/<path:filename>')
 def static_files(filename):
     return send_from_directory(app.config['STATIC_FOLDER'], filename)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
